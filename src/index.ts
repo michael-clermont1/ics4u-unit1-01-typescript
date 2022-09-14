edd5359e96f4c8c62a4d1060805cecfb90560e84
Michael Clermont <michael.clermont@mths.ca>
2022-09-14 00:57:13 +0000

    edited weight program

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ import promptSync from 'prompt-sync'
 const prompt = promptSync()
 
 const lengthString = prompt('What is the length of the log (0.25, 0.5 or 1): ')
-let lengthNumber = Number(lengthString)
+const lengthNumber = Number(lengthString)
 console.log(`The log length is ${lengthNumber}m.`)
 
 let weight = lengthNumber * 20
