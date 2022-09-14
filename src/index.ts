8b0c2dc1572f526906a992b0d6047e24c1f96695
michael-clermont1 <michael-clermont1@users.noreply.github.com>
2022-09-14 00:57:06 +0000

    Prettified Code!

--- src/index.ts
+++ src/index.ts
@@ -7,13 +7,9 @@
  * Since:   2022-11-25
  */
 
-
 import promptSync from 'prompt-sync'
 
-
 const prompt = promptSync()
-
-
 
 const lengthString = prompt('What is the length of the log (0.25, 0.5 or 1): ')
 let lengthNumber = Number(lengthString)
@@ -22,6 +18,5 @@ console.log(`The log length is ${lengthNumber}m.`)
 let weight = lengthNumber * 20
 weight = 1100 / weight
 console.log(`The amount of logs that can fit in the truck is ${weight}.`)
-
 
 console.log('\nDone.')
