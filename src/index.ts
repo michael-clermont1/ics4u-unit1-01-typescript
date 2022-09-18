11667026a0f24a5b0492d598d7c5d7930b40284f
Michael Clermont <michael.clermont@mths.ca>
2022-09-18 14:21:44 +0000

    edited weight program

--- src/index.ts
+++ src/index.ts
@@ -10,13 +10,15 @@
 import promptSync from 'prompt-sync'
 
 const prompt = promptSync()
+const logWeight = 20
+const truckCarryLimit = 1100
 
 const lengthString = prompt('What is the length of the log (0.25, 0.5 or 1): ')
 const lengthNumber = Number(lengthString)
 console.log(`The log length is ${lengthNumber}m.`)
 
-let weight = lengthNumber * 20
-weight = 1100 / weight
+let weight = lengthNumber * logWeight
+weight = truckCarryLimit / weight
 console.log(`The amount of logs that can fit in the truck is ${weight}.`)
 
 console.log('\nDone.')
